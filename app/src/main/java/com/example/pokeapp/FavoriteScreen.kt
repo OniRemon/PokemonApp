c7f62dc108a6ad315870fd542827a9a8eb6c23ee
Kenta Saito <saito4431@gmail.com>
2024-10-17 23:12:09 +0900

    BottomBar仮実装

---
+++ app/src/main/java/com/example/pokeapp/FavoriteScreen.kt
@@ -0,0 +1,9 @@
+package com.example.pokeapp
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FavoriteScreen() {
+    Text(text = "お気に入り画面")
+}
--- app/src/main/java/com/example/pokeapp/MainActivity.kt
+++ app/src/main/java/com/example/pokeapp/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.pokeapp.ui.theme.PokeAppTheme
 
@@ -22,16 +30,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PokeAppTheme {
+                val navController = rememberNavController()
                 Scaffold(
-                    modifier = Modifier.fillMaxSize()
+                    bottomBar =   {
+                        BottomBar(navController = navController)
+                    }
                 ) { innerPadding ->
-                    val navController = rememberNavController()
                     NavHost(
                         navController = navController,
-                        startDestination = ScreenRoute.SearchScreen.route,
+                        startDestination = BottomNavItem.Home.route,
                         modifier = Modifier.padding(innerPadding)
                     ) {
-                        composable(route = ScreenRoute.SearchScreen.route) {
+                        composable(BottomNavItem.Home.route) {
+                            HomeScreen()
+                        }
+                        composable(BottomNavItem.Favorite.route) {
+                            FavoriteScreen()
+                        }
+                        composable(BottomNavItem.Profile.route) {
+                            ProfileScreen()
+                        }
+                        composable(route = BottomNavItem.Search.route) {
                             SearchScreen(navController = navController)
                         }
                         composable(route = "${ScreenRoute.ResultScreen.route}/{id}") { backStackEntry ->
@@ -41,6 +60,41 @@ class MainActivity : ComponentActivity() {
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun BottomBar(navController: NavController) {
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.Search,
+        BottomNavItem.Favorite,
+        BottomNavItem.Profile
+    )
+    NavigationBar {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        items.forEach { item ->
+            NavigationBarItem(
+                icon = { Icon(item.icon, contentDescription = item.title) },
+                label = { Text(item.title) },
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = Color.Blue,
+                    unselectedIconColor = Color.Gray
+                )
+            )
         }
     }
 }
